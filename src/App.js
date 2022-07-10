67f1aeb1ae99dcb489c3a6baf7cd648ae0a95645
SusRedDev <meliani.yessine@gmail.com>
2022-07-10 14:56:02 +0100

    v2

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from "./components/assets/logo.png"
+
+const App = () => {
+  return (
+    <>
+    <img src={logo} alt='Logo' />
+    </>
+  )
+}
+
+export default App
