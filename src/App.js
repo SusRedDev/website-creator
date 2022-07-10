dc4f67d55df4cf365dd29186a48b77bf52da521b
SusRedDev <meliani.yessine@gmail.com>
2022-07-10 15:16:14 +0100

    v3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import logo from "./components/assets/logo.png"
+import Button from "@material-ui/core/Button"
 
 const App = () => {
   return (
     <>
+    <Button 
+      onClick={() => alert("Hello")}
+      variant='contained'
+      color='secondary'
+    >
+      HELLO WORLD
+    </Button>
     <img src={logo} alt='Logo' />
     </>
   )
